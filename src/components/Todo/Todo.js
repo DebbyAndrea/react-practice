2dfbaeb73737225471e9ea138c48fcd3ee95e708
Andrea J <750951@cognizant.com>
2020-02-10 10:45:58 +0530

    some changes

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -5,7 +5,7 @@ function Todo(props) {
     return (
         <div className="todo-item">
             <input type="checkbox" onChange={() => props.checkBoxChange(props.todoList.id)}/>
-            <p>{props.todoList.text}</p>
+            <p style={{textDecoration:props.todoList.completed?"line-through":"none"}}>{props.todoList.text}</p>
             <span className="close" onClick={() => props.deleteTodo(props.todoList.id)}>&times;</span>
         </div>
       
